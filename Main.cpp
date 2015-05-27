f61f1c91625d0283d9fe8ec00ba036a098090e6e
Marek Dědič <developer@dedic.eu>
2015-05-27 10:25:32 +0000

    Simple bug

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    BigInt test{new BigInt{-INT_MAX}};
+    BigInt test{-INT_MAX};
     BigInt test2{-INT_MAX};
     cout << "Hello World!" << endl;
     cout << "BigInt: " << test - test2 << endl;
