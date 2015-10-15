e877023f84aa52aa6167ea19ec757c156343c693
Marek Dědič <developer@dedic.eu>
2015-10-15 22:39:39 +0000

    Exceptions locally

--- src/BigInt.cpp
+++ src/BigInt.cpp
@@ -6,58 +6,97 @@ BigInt::BigInt() : digits{}
 
 BigInt::BigInt(int input) : digits{}
 {
-    bool negative{input < 0};
-    for(unsigned int i{0}; i < 8 * sizeof(int); i++)
+    try
     {
-        digits.push_back(static_cast<bool>(input % 2));
-        input = input / 2;
+        bool negative{input < 0};
+        for(unsigned int i{0}; i < 8 * sizeof(int); i++)
+        {
+            digits.push_back(static_cast<bool>(input % 2));
+            input = input / 2;
+        }
+        if(negative)
+        {
+            *this = -(*this);
+        }
     }
-    if(negative)
+    catch(bad_alloc& e)
     {
-        *this = -(*this);
+        cerr << e.what() << endl;
+        abort(); 
+    }
+    catch(...)
+    {
+        cerr << "Unknown error" << endl;
+        abort();
     }
 }
 
 BigInt BigInt::operator-() const                                                // Two's complement
 {
-    BigInt ret{};
-    ret.digits.reserve(digits.size());
-    bool flag{false};
-    for(vector<bool>::const_iterator it{digits.begin()}; it != digits.end(); ++it)
+    try
     {
-        if(flag)
+        BigInt ret{};
+        ret.digits.reserve(digits.size());
+        bool flag{false};
+        for(vector<bool>::const_iterator it{digits.begin()}; it != digits.end(); ++it)
         {
-            ret.digits.push_back(!*it);
-        }
-        else
-        {
-            ret.digits.push_back(*it);
-            if(*it == true)
+            if(flag)
             {
-                flag = true;
+                ret.digits.push_back(!*it);
+            }
+            else
+            {
+                ret.digits.push_back(*it);
+                if(*it == true)
+                {
+                    flag = true;
+                }
             }
         }
+        return ret;
     }
-    return ret;
+    catch(bad_alloc& e)
+    {
+        cerr << e.what() << endl;
+        abort(); 
+    }
+    catch(...)
+    {
+        cerr << "Unknown error" << endl;
+        abort();
+    }
 }
 
 BigInt& BigInt::operator+=(const BigInt& other)
 {
-    bool carryOut{};
-    bool carryIn{};
-    bool temp{};
-    for(unsigned int i{0}; i != other.digits.size(); i++)
+    try
     {
-        carryIn = carryOut;
-        temp = digits[i] xor other.digits[i];
-        carryOut = (temp and carryIn) or (digits[i] and other.digits[i]);
-        digits[i] = temp xor carryIn;
+        bool carryOut{};
+        bool carryIn{};
+        bool temp{};
+        for(unsigned int i{0}; i != other.digits.size(); i++)
+        {
+            carryIn = carryOut;
+            temp = digits[i] xor other.digits[i];
+            carryOut = (temp and carryIn) or (digits[i] and other.digits[i]);
+            digits[i] = temp xor carryIn;
+        }
+        if(carryOut xor carryIn)
+        {
+            digits.push_back(carryOut);
+        }
+        return *this;
     }
-    if(carryOut xor carryIn)
+    catch(bad_alloc& e)
     {
-        digits.push_back(carryOut);
+        cerr << e.what() << endl;
+        abort(); 
     }
-    return *this;
+    catch(...)
+    {
+        cerr << "Unknown error" << endl;
+        abort();
+    }
 }
 
 const BigInt BigInt::operator+(const BigInt& other) const
@@ -82,71 +121,84 @@ const BigInt BigInt::operator-(const BigInt& other) const
 
 BigInt& BigInt::operator*=(const BigInt& other)
 {
-    unsigned int x = digits.size();
-    unsigned int y = other.digits.size();
-    BigInt neg = -*this;
-    vector<bool> A{};
-    vector<bool> S{};
-    vector<bool> P{};
-
-    // Fill
-    P.push_back(false);
-    for(unsigned int i{0}; i < y + 1; i++)
+    try
     {
-        A.push_back(false);
-        S.push_back(false);
-    }
-    for(unsigned int i{0}; i < x; i++)
-    {
-        A.push_back(digits[i]);
-        S.push_back(neg.digits[i]);
-    }
-     for(unsigned int i{0}; i < y; i++)
-    {
-        P.push_back(other.digits[i]);
-    }
-    for(unsigned int i{0}; i < x; i++)
-    {
+        unsigned int x = digits.size();
+        unsigned int y = other.digits.size();
+        BigInt neg = -*this;
+        vector<bool> A{};
+        vector<bool> S{};
+        vector<bool> P{};
+    
+        // Fill
         P.push_back(false);
+        for(unsigned int i{0}; i < y + 1; i++)
+        {
+            A.push_back(false);
+            S.push_back(false);
+        }
+        for(unsigned int i{0}; i < x; i++)
+        {
+            A.push_back(digits[i]);
+            S.push_back(neg.digits[i]);
+        }
+         for(unsigned int i{0}; i < y; i++)
+        {
+            P.push_back(other.digits[i]);
+        }
+        for(unsigned int i{0}; i < x; i++)
+        {
+            P.push_back(false);
+        }
+    
+        //Booth's algorithm
+        bool carryOut{};
+        bool carryIn{};
+        bool temp{};
+        for(unsigned int i = 0; i < y; i++)
+        {
+            carryOut = false;
+            if(P[0] and !P[1])
+            {
+                // P += A
+                for(unsigned int i{0}; i != A.size(); i++)
+                {
+                    carryIn = carryOut;
+                    temp = P[i] xor A[i];
+                    carryOut = (temp and carryIn) or (P[i] and A[i]);
+                    P[i] = temp xor carryIn;
+                }
+            }
+            if(!P[0] and P[1])
+            {
+                // P += S
+                for(unsigned int i{0}; i != S.size(); i++)
+                {
+                    carryIn = carryOut;
+                    temp = P[i] xor S[i];
+                    carryOut = (temp and carryIn) or (P[i] and S[i]);
+                    P[i] = temp xor carryIn;
+                }
+            }
+            for(unsigned int i{0}; i != P.size() - 1; i++)
+            {
+                P[i] = P[i + 1];
+            }
+        }
+        P.erase(P.begin());
+        digits = P;
+        return *this;
     }
-
-    //Booth's algorithm
-    bool carryOut{};
-    bool carryIn{};
-    bool temp{};
-    for(unsigned int i = 0; i < y; i++)
+    catch(bad_alloc& e)
     {
-        carryOut = false;
-        if(P[0] and !P[1])
-        {
-            // P += A
-            for(unsigned int i{0}; i != A.size(); i++)
-            {
-                carryIn = carryOut;
-                temp = P[i] xor A[i];
-                carryOut = (temp and carryIn) or (P[i] and A[i]);
-                P[i] = temp xor carryIn;
-            }
-        }
-        if(!P[0] and P[1])
-        {
-            // P += S
-            for(unsigned int i{0}; i != S.size(); i++)
-            {
-                carryIn = carryOut;
-                temp = P[i] xor S[i];
-                carryOut = (temp and carryIn) or (P[i] and S[i]);
-                P[i] = temp xor carryIn;
-            }
-        }
-        for(unsigned int i{0}; i != P.size() - 1; i++)
-        {
-            P[i] = P[i + 1];
-        }
+        cerr << e.what() << endl;
+        abort(); 
     }
-    P.erase(P.begin());
-    digits = P;
-    return *this;
+    catch(...)
+    {
+        cerr << "Unknown error" << endl;
+        abort();
+    }
 }
 
 const BigInt BigInt::operator*(const BigInt& other) const
--- src/Main.cpp
+++ src/Main.cpp
@@ -3,24 +3,12 @@ using namespace std;
 
 int main()
 {
-    try
-    {
-        BigInt test{236};
-        BigInt test2{482};
-        cout << "Hello World!" << endl;
-        cout << "BigInt: " << test * test2 << endl;
-        cout << "Bye!" << endl;
-    }
-    catch(bad_alloc& e)
-    {
-        cerr << e.what() << endl;
-        return -1; 
-    }
-    catch(...)
-    {
-        cerr << "Unknown error" << endl;
-        return -1;
-    }
+    BigInt test{236};
+    BigInt test2{142424524};
+    cout << "Hello World!" << endl;
+    cout << "Sum: " << test + test2 << endl;
+    cout << "Product: " << test * test2 << endl;
+    cout << "Bye!" << endl;
 
     return 0;
 }
